3e55dcebf41fe60e50c83c5f9cea9dd8c7d7fd4d
ComputerCow <mjacobs2@wpi.edu>
2025-03-15 18:23:17 -0400

    setup for events

--- main.lua
+++ main.lua
@@ -1,208 +1,260 @@
 if not BG then BG = {} end
 BG.bingo_active=true
+BG.Progress = {}
+BG.Gameplay = {}
 BG.UI = {}
 BG.Challenges = {
   {
     name="4 Eights",
-    text={
+    text=function() return {
       "Play a hand",
       "of 4 eights."
-    }
+    } end
   },
   {
     name="x4 Mult",
-    text={
+    text=function() return {
       "Gain x4 mult",
       "from a single",
       "joker trigger."
-    }
+    } end
   },
   {
     name="Narrow Deck",
-    text={
+    text=function() return {
       "Have 35 or",
       "fewer cards",
       "in your deck."
-    }
+    } end
   },
   {
     name="Big Deck",
-    text={
+    text=function() return{
       "Have 70 or more",
       "cards in your",
       "deck."
-    }
+    } end
   },
   {
     name="Early Vouchers",
-    text={
+    text=function() return {
       "Buy 3 vouchers",
-      "before ante 4."
-    }
+      "before ante 4.",
+      "(" .. tostring(BG.Progress["Early Vouchers"].num_early_vouchers) .. " bought so far.)"
+    } end,
+    --If there's a countable thing within a round, set it up in setup
+    setup=function ()
+      BG.Progress["Early Vouchers"].num_early_vouchers=0
+    end
   },
   {
     name="No Early Jokers",
-    text={
+    text=function() return {
       "Clear ante 2",
       "before obtaining",
       "any jokers."
-    }
+    } end,
+    setup=function ()
+      BG.Progress["No Early Jokers"].early_jokers_bought=false
+    end,
+    --If a condition could make this impossible in the current run, check here.
+    impossible=function()
+      return BG.Progress["No Early Jokers"].early_jokers_bought
+    end
   },
   {
     name="Win",
-    text={
+    text=function() return {
       "Clear ante 8."
-    }
+    } end
   },
   {
     name="Disable Boss",
-    text={
+    text=function() return {
       "Disable a",
       "boss blind."
-    }
+    } end
   },
   {
     name="Retriggers",
-    text={
+    text=function() return {
       "Retrigger 5",
       "cards in a",
       "single hand."
-    }
+    } end,
+    setup=function ()
+      BG.Progress["Retriggers"].retriggers_current_hand=0
+    end
   },
   {
     name="Debt",
-    text={
+    text=function() return {
       "Go into $15",
       "of debt."
-    }
+    } end,
   },
   {
     name="Skip",
-    text={
+    text=function() return {
       "Skip all small",
       "and large blinds",
       "for 3 consecutive",
-      "antes."
-    }
+      "antes.",
+      "(" .. tostring(BG.Progress["Skip"].consecutive_blinds_skipped) .. " skipped so far.)"
+    } end,
+    setup=function()
+      BG.Progress["Skip"].consecutive_blinds_skipped=0
+    end
   },
   {
     name="Rich",
-    text={
+    text=function() return {
       "Have $150."
-    }
+    } end
   },
   {
     name="Big Sale",
-    text={
+    text=function() return {
       "Sell a joker",
       "for $10 or more."
-    }
+    } end
   },
   {
     name="Royal Flush",
-    text={
+    text=function() return {
       "Play a Royal Flush."
-    }
+    } end
   },
   {
     name="Five of a Kind",
-    text={
+    text=function() return {
       "Play a Five",
       "of a Kind."
-    }
+    } end
   },
   {
     name="Stocks",
-    text={
+    text=function() return {
       "Gain $25 at",
       "the end of a",
       "round."
-    }
+    } end
   },
   {
     name="Single Tap",
-    text={
+    text=function() return {
       "Beat a boss blind",
       "by playing a single",
       "high card."
-    }
+    } end
   },
   {
     name="Pretty High Level",
-    text={
+    text=function() return {
       "Get a poker hand",
       "to level 15."
-    }
+    } end
   },
   {
     name="Rising Power",
-    text={
+    text=function() return {
       "Upgrade the same",
       "poker hand 3 times",
       "in one round."
-    }
+    } end,
+    setup=function ()
+      --TODO: Populate table with each type of hand
+      BG.Progress["Rising Power"].poker_hands_upgraded_this_round = {}
+    end
   },
   {
     name="High Score",
-    text={
+    text=function() return {
       "Score 2 million",
       "points from a single",
       "hand."
-    }
+    } end
   },
   {
     name="Rarity",
-    text={
-      "Have 4 rare cards."
-    }
+    text=function() return {
+      "Have 4 rare jokers."
+    } end
   },
   {
     name="Steely",
-    text={
+    text=function() return {
       "Trigger 5 steel",
       "cards in one hand."
-    }
+    } end,
+    setup=function ()
+      BG.Progress["Steely"].steel_cards_triggered_current_hand=0
+    end
   },
   {
     name="Lucky",
-    text={
+    text=function() return {
       "Trigger 10",
-      "lucky cards."
-    }
+      "lucky cards.",
+      "(" .. tostring(BG.Progress["Lucky"].lucky_cards_triggered) .. " triggered so far.)"
+    } end,
+    setup=function ()
+      BG.Progress["Lucky"].lucky_cards_triggered=0
+    end
   },
   {
     name="Chippy",
-    text={
+    text=function() return {
       "Get 500 chips",
       "from one hand."
-    }
+    } end
   },
   {
     name="Seals",
-    text={
+    text=function() return {
       "Play a hand",
       "containing every",
       "seal."
-    }
+    } end
   }
 }
 
-function BG.UI.get_challenges()
+BG.Gameplay.active_challenges = {}
+
+function BG.Gameplay.get_challenges()
   -- TODO: Shuffle then return first 25.
   return {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25}
+end
+
+function BG.Gameplay.setup_challenges()
+  local challenge_numbers = BG.Gameplay.get_challenges()
+  BG.Gameplay.active_challenges = {}
+  for index, value in ipairs(challenge_numbers) do
+    local challenge = BG.Challenges[value]
+    sendTraceMessage("Setting up progress for " .. challenge.name, "BalingoLog")
+    BG.Progress[challenge.name]={
+      completed=false,
+      impossible=false
+    }
+    if challenge.setup ~= nil then challenge.setup() end
+    BG.Gameplay.active_challenges[index]=challenge
+  end
 end
 
 function BG.UI.get_challenge_box(index)
   local children = {}
   sendTraceMessage("Got to " .. tostring(index), "BingoLog")
-  for i,text in pairs(BG.Challenges[index].text) do
+  local challenge = BG.Challenges[index]
+  local progress = BG.Progress[challenge.name]
+  local colour = progress.completed and G.C.GREEN or (progress.impossible and G.C.RED or G.C.BLACK)
+  for i,text in ipairs(challenge.text()) do
     table.insert(children,{
       n=G.UIT.R,
       config={},
       nodes={
         {
           n=G.UIT.T,
-          config={align="tl",text=text,colour=G.C.BLACK,scale=0.35}
+          config={align="tl",text=text,colour=colour,scale=0.35}
         }
       }
     })
@@ -252,7 +304,7 @@ function G.UIDEF.run_info()
 end
 
 function BG.UI.BoardDisplay()
-  local ch = BG.UI.get_challenges()
+  local ch = BG.Gameplay.get_challenges()
   return {
     n=G.UIT.ROOT, 
     config={align="tl", minw=3, padding = 0.1, r=0.1, color=G.C.CLEAR},
@@ -314,4 +366,12 @@ function BG.UI.BoardDisplay()
       }
     }
   }
+end
+
+local check_for_unlock_old = check_for_unlock
+-- When adding new things, add new types to check_for_unlock so all the logic stays together
+check_for_unlock = function(args)
+  local ret = check_for_unlock_old(args)
+  
+  return ret
 end
